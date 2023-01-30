2f371538e0ca5eac2aac1cee221ffdda3dc786a8
C M Santosh Kumar <pagadala.kalyani@gmail.com>
2023-01-30 13:08:14 +0000

    code added for 'delete' route to the application

--- server.js
+++ server.js
@@ -46,7 +46,23 @@ app.get('/api/notes', (rew,res) =>
    res.json(newNote);
  })
    
+ function deleteNote(id, notesArray){
+   for(let i=0; i<notesArray.length; i++){
+     let note = notesArray[i];
+
+      if(note.id == id){
+         notesArray.splice(i, 1);
+         fs.appendFileSync(path.json(__dirname, './db/db.json'),
+          JSON.stringify(notesArray, null, 2));
+          break;
+      }
+   }
+ }
  
+ app.delete('/api/notes/:id', (req, res) =>{
+   deleteNote(req.params.id, allNotes);
+   res.json(true);
+ });
 
  app.listen(PORT, () =>
   console.log(`App listening at http:localhost:${PORT} 🚀`)
