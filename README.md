ee133d6b81f56acb7c89a2da2ee16ee3e2c133dc
C M Santosh Kumar <pagadala.kalyani@gmail.com>
2023-02-02 22:18:25 +0000

    Readme file updated with deplyoed url and screenshots, code modified for the id, delete request added and comments added to the code

--- README.md
+++ README.md
@@ -1,24 +1,39 @@
 # Project Title: Note-Taker
-An application called Note Taker that can be used to write and save notes.
 
 ## Project Description:
-This is an application called Note Taker that can be used to write and save notes by using an Express.js back end and can save and retrieve note data from a JSON file. Starter code has front end application.
+This application is known as a Note Taker. It can be used to write and save notes, using an Express.js backend framework. It saves and retrieves noted data from a JSON file. Starter code has a frontend application.
 
 ## Installation:
-  This application runs through Node.js. It needs the `express` back end dipendencies are installed using `npm i` or `npm install` commands. Installed `Heroku` for deploying the application.
+  This application runs through Node.js. It needs the `express.js` backend dipendencies that are installed using either `npm i` or `npm install` commands. `Heroku` was installed for deploying the application.
 
 ## Usage:
    The application can be invoked using a `node server.js` command.
 
    ### Deployed application URL from Heroku:
-    
-    
-   
+
+    https://intense-chamber-44315.herokuapp.com/ 
+  
       
+   The following screenshots depict the generated website. 
 
-   The following screenshot depicts the generated website. 
 
-   ![screenshot-of-applicaton](./dist/img1.png)
+
+   ![screenshot-of-applicaton](./public/assets/images/img1.png)
+
+
+
+
+
+   ![screenshot-of-applicaton](./public/assets/images/img2.png)
+
+
+
+
+
+   ![screenshot-of-applicaton](./public/assets/images/img3.png)
+
+
+
 
 
 ## License:
@@ -31,3 +46,9 @@ Read more about the MIT licence here: https://opensource.org/licenses/MIT.
 
  ## Credits:
   Thanks to Xander Rapstine (https://github.com/Xandromus), John McCambridge (https://github.com/nol166) and George (https://github.com/Georgeyoo) for providing rescources for this project.
+
+## Questions?
+  ### Reach me here: 
+   My GitHub profile:  [veerak21](https://github.com/veerak21),
+
+   My E-mail: pv.kallu@gmail.com.  
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const allNotes = require('./db/db.json');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -14,10 +15,12 @@ app.get('/api/notes', (req,res) =>
  res.json(allNotes.slice(1))
 );
 
+ // GET Route for homepage
  app.get('./', (req,res) =>
  res.sendFile(path.join(__dirname, '/public/index.html'))
  );
 
+ // GET Route for notes page
  app.get('/notes', (req,res)=>
  res.sendFile(path.join(__dirname,'/public/notes.html'))
  );
@@ -26,26 +29,30 @@ app.get('/api/notes', (req,res) =>
  res.sendFile(path.join(__dirname,'/public/index.html'))
  );
 
+ // Creating the new note
  function addNewNote(body, notesArray){
+  console.log(body, "body");
+  console.log(notesArray,"notesArray");
     const newNote = body;
     if(!Array.isArray(notesArray))
       notesArray = [];
-      if(notesArray.length === 0)
-      body.id = notesArray[0];
-      notesArray[0]++;
+      newNote.id = notesArray.length + 1;
+     console.log(body); 
       notesArray.push(newNote);
-
+     // Write the new note to a file
     fs.writeFileSync(path.join(__dirname, './db/db.json'),
+    // Convert the data to a string so we can save it
       JSON.stringify(notesArray, null, 2)
     );
     return newNote;  
  }
-
+ // POST request to add new note
  app.post('/api/notes', (req,res) => {
    const newNote = addNewNote(req.body, allNotes);
    res.json(newNote);
  })
    
+ //Deleting the note
  function deleteNote(id, notesArray){
 
    for(let i=0; i<notesArray.length; i++){
@@ -53,17 +60,19 @@ app.get('/api/notes', (req,res) =>
       if(note.id == id){
          notesArray.splice(i, 1);
          fs.writeFileSync(path.join(__dirname, './db/db.json'),
+         // Convert the data to a string so we can save it
           JSON.stringify(notesArray, null, 2));
           break;
       }
    }
  }
- 
+ // Delete request for a note based on id
  app.delete('/api/notes/:id', (req, res) =>{
    deleteNote(req.params.id, allNotes);
    res.json(true);
  });
 
+ //It binds and listens to the connections on the specified host and port
  app.listen(PORT, () =>
   console.log(`App listening at http:localhost:${PORT} 🚀`)
  )
